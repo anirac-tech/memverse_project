34f5e4a68b2d13f9480bb83637b6563d32f2429d
neil warner <njwandroid@gmail.com>
2025-04-06 01:18:21 -0500

    update app test to use fake verse repository

--- test/step/the_app_is_running.dart
+++ test/step/the_app_is_running.dart
@@ -1,7 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:memverse/src/app/app.dart';
+import 'package:memverse/src/features/verse/data/verse_repository.dart';
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
-  await tester.pumpWidget(const ProviderScope(child: App()));
+  await tester.pumpWidget(
+    ProviderScope(
+      overrides: [verseRepositoryProvider.overrideWith((ref) => FakeVerseRepository())],
+      child: const App(),
+    ),
+  );
 }
