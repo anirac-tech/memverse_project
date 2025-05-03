c6eea1b46efc37c339180155656c0daea6fb1dd0
neil warner <njwandroid@gmail.com>
2025-05-03 16:39:46 -0500

    remove unnecessary script and replace with direct formatting

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,14 +1,6 @@
-import 'dart:developer';
-
 import 'package:memverse/src/app/app.dart';
 import 'package:memverse/src/bootstrap.dart';
-import 'package:memverse/src/utils/app_logger.dart';
 
 void main() {
-  // Use the CLIENT_ID from dart-define parameter
-  AppLogger.d(
-    'Pipeline check - MEM-115 custom lint package moved to separate ticket',
-    name: 'memverse.pipeline',
-  );
   bootstrap(() => const App());
 }
