14c08b5a326243e1b280e8f9c3d15a1f5fd714b4
neil warner <njwandroid@gmail.com>
2025-06-03 08:56:41 -0500

    posthog poc wip android

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:memverse/src/app/app.dart';
 import 'package:memverse/src/bootstrap.dart';
+import 'package:memverse/src/utils/app_logger.dart';
+import 'package:posthog_flutter/posthog_flutter.dart';
 
-void main() {
+bool hasPostHog = false;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const apiKey = String.fromEnvironment('POSTHOG_MEMVERSE_API_KEY');
+  if (apiKey.isEmpty) {
+    AppLogger.e('Error: POSTHOG_MEMVERSE_API_KEY environment variable is not set');
+  } else {
+    if (kDebugMode) {
+      AppLogger.d('PostHog API Key: $apiKey');
+    }
+    final config = PostHogConfig(apiKey);
+    config.host = 'https://us.i.posthog.com';
+    config.debug = true;
+    config.captureApplicationLifecycleEvents = true;
+
+    // check https://posthog.com/docs/session-replay/installation?tab=Flutter
+    // for more config and to learn about how we capture sessions on mobile
+    // and what to expect
+    config.sessionReplay = true;
+    // choose whether to mask images or text
+    config.sessionReplayConfig.maskAllTexts = false;
+    config.sessionReplayConfig.maskAllImages = false;
+
+    // Setup PostHog with the given Context and Config
+    await Posthog().setup(config);
+    hasPostHog = true;
+  }
+
   bootstrap(() => const App());
 }
