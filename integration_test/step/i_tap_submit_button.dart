840720b9b419ea0413cf0323d0ee1033539db561
neil warner <njwandroid@gmail.com>
2025-06-04 01:11:37 -0500

    test steps

--- integration_test/step/i_tap_submit_button.dart
+++ integration_test/step/i_tap_submit_button.dart
@@ -1,6 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 /// Usage: I tap submit button
 Future<void> iTapSubmitButton(WidgetTester tester) async {
-  throw UnimplementedError();
+  // Wait for the page to be ready
+  await tester.pumpAndSettle();
+
+  // Find and tap the submit button
+  final submitButton = find.byKey(const Key('submit-ref'));
+  expect(submitButton, findsOneWidget);
+
+  await tester.tap(submitButton);
+
+  // Pump to register the tap
+  await tester.pump();
 }
--- integration_test/step/the_app_is_running.dart
+++ integration_test/step/the_app_is_running.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:memverse/main.dart';
+import 'package:memverse/main_development.dart' as app;
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
-  await tester.pumpWidget(MyApp());
+  await app.main();
+  await tester.pumpAndSettle();
 }
